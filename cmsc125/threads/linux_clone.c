7c6947504b539731fa52c4c11bb0558fb6ccd2a6
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-03-07 08:53:51 +0800

    edits

---
+++ cmsc125/threads/linux_clone.c
@@ -0,0 +1,50 @@
+// https://eli.thegreenplace.net/2018/launching-linux-threads-and-processes-with-clone/
+
+#define _GNU_SOURCE
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+
+static int child_func(void* arg) {
+  char* buf = (char*)arg;
+  printf("Child sees buf = \"%s\"\n", buf);
+  strcpy(buf, "hello from child");
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  // Allocate stack for child task.
+  const int STACK_SIZE = 65536;
+  char* stack = malloc(STACK_SIZE);
+  if (!stack) {
+    perror("malloc");
+    exit(1);
+  }
+
+  // When called with the command-line argument "vm", set the CLONE_VM flag on.
+  unsigned long flags = 0;
+  if (argc > 1 && !strcmp(argv[1], "vm")) {
+    flags |= CLONE_VM;
+  }
+
+  char buf[100];
+  strcpy(buf, "hello from parent");
+  if (clone(child_func, stack + STACK_SIZE, flags | SIGCHLD, buf) == -1) {
+    perror("clone");
+    exit(1);
+  }
+
+  int status;
+  if (wait(&status) == -1) {
+    perror("wait");
+    exit(1);
+  }
+
+  printf("Child exited with status %d. buf = \"%s\"\n", status, buf);
+  return 0;
+}
