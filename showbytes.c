c5d8ed919ac81cedf185d242975da52e92dc24c0
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-08-28 01:33:31 +0800

    edits

--- showbytes.c
+++ showbytes.c
@@ -12,8 +12,26 @@ void show_bytes(pointer start, int len){
 }
 
 int main(){
-   int a=10;
+   int a=15213;
+   int b=-15213;
+   long c=15213;
+   long *lp=&c;
+   int *ip=&b;
+
+
    printf("int a = 15213;\n");
    show_bytes((pointer)&a,sizeof(int));
+
+   printf("int b = -15213;\n");
+   show_bytes((pointer)&b,sizeof(int));
+
+   printf("long c = 15213;\n");
+   show_bytes((pointer)&c,sizeof(long));
+   
+   printf("long *lp = &c;\n");
+   show_bytes((pointer)&c,sizeof(long *));
+
+   printf("int *ip = &b;\n");
+   show_bytes((pointer)&c,sizeof(int *));
 }
 
