501a1db2dcfdc0d54f093cfff77d15a40f758220
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-08-29 20:40:29 +0800

    edits

---
+++ promotion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   printf("%d\n", (0 == 0U));
+   printf("%d\n", (-1 < 0));
+   printf("%d\n", (-1 < 0U));
+   printf("%d\n", (2147483647 > (-2147483647-1)));
+   printf("%d\n", (2147483647U > (-2147483647-1)));
+   printf("%d\n", (2147483647 > ((int)2147483647U)));
+   printf("%d\n", (-1 > -2));
+   printf("%d\n", ((unsigned) -1 > -2 ));
+
+}
