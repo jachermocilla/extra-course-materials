a24688b6ca9ad5a2b6dd61de4f21553841e9385f
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-08-29 20:46:09 +0800

    edits

--- expansion.c
+++ expansion.c
@@ -17,9 +17,13 @@ int main(){
    int x = sx;
    unsigned ux = usx;
 
+   printf("sx= %d\n",sx);
    show_bytes((pointer)&sx,sizeof(short));
+   printf("sx= %u\n",usx);
    show_bytes((pointer)&usx,sizeof(unsigned short));
+   printf("x= %d\n",x);
    show_bytes((pointer)&x,sizeof(int));
+   printf("ux= %u\n",ux);
    show_bytes((pointer)&ux,sizeof(unsigned));
 }
 
