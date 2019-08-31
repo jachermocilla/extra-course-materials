2a2c303adf23889a8161816f24812bc841155e44
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-08-31 23:21:47 +0800

    edits

---
+++ acharstudy.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef unsigned char *pointer;
+
+
+void show_bytes(pointer start, int len){
+   int i, c, k;
+
+   for (i=0;i<len;i++){
+      printf("0x%.2X ",start[i]);
+   }
+   
+   //binary
+   for (i=0;i<len;i++){
+      for (c = 7; c >= 0; c--){
+         k = start[i] >> c;
+         if (k & 1)
+            printf("1");
+         else
+            printf("0");
+      }
+      printf("\n");
+   }
+}
+
+int main(){
+   unsigned char uc;
+   char sc;
+
+   for (uc=0; uc<UCHAR_MAX; uc++){
+      sc = uc;
+   
+      //print the actual values
+      printf("%d %d\n",uc,sc);
+
+      //print the internal representation
+      show_bytes((pointer)&uc,sizeof(unsigned char));
+      show_bytes((pointer)&sc,sizeof(char));
+
+      printf("\n\n");
+   }
+
+}
+
+//It can be observed that the internal representations are the same
+//but the interpretations are different for signed and unsigned
