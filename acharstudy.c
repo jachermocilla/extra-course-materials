851d97372e7486809b3cead128048b2c5b02eb17
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-09-03 14:35:08 +0800

    edits

--- acharstudy.c
+++ acharstudy.c
@@ -47,7 +47,7 @@ int main(){
    short ssa;
 
 
-   //shows the correspondence between signed and unsigned 
+   printf("//shows the correspondence between signed and unsigned\n"); 
    for (uc=0; uc<UCHAR_MAX; uc++){
       sc = uc;
    
@@ -60,14 +60,14 @@ int main(){
       printf("\n\n");
    }
 
-   //expansion unsigned
+   printf("//expansion unsigned\n");
    usa = uca;
    show_bytes((pointer)&uca,sizeof(unsigned char));
    show_bytes((pointer)&usa,sizeof(unsigned short));
    
    printf("\n");
    
-   //expansion signed
+   printf("//expansion signed\n");
    ssa = sca;
    show_bytes((pointer)&sca,sizeof(char));
    show_bytes((pointer)&ssa,sizeof(short));
@@ -75,32 +75,32 @@ int main(){
    printf("\n");
 
 
-   //truncate unsigned
+   printf("//truncate unsigned\n");
    usa = 260;
    uca = usa; 
    show_bytes((pointer)&usa,sizeof(unsigned short));
    show_bytes((pointer)&uca,sizeof(unsigned char ));
 
-   //truncation is equivalent to getting the mod 2^w
+   printf("//truncation is equivalent to getting the mod 2^w\n");
    unsigned short ustmp = usa % (1 << 8);
    show_bytes((pointer)&ustmp,sizeof(unsigned short));
    
    printf("\n");
 
    
-   //truncate signed
+   printf("//truncate signed\n");
    ssa = -260;
    sca = ssa; 
    show_bytes((pointer)&ssa,sizeof(signed short));
    show_bytes((pointer)&sca,sizeof(signed char ));
    
-   //truncation is equivalent to getting the mod 2^w but treated as signed
+   printf("//truncation is equivalent to getting the mod 2^w but treated as signed\n");
    short sstmp = ssa % (1 << 8);
    show_bytes((pointer)&sstmp,sizeof(short));
    
    printf("\n");
 
-   //complement and increment
+   printf("//complement and increment\n");
 
    sca=0x6D;
    scb=~sca;
@@ -109,7 +109,7 @@ int main(){
    show_bytes((pointer)&scb,sizeof(signed char ));
    show_bytes((pointer)&sctmp,sizeof(signed char ));
 
-   //the case for zero
+   printf("//the case for zero\n");
    sca=0;
    scb=~0;
    sctmp=scb+1;
@@ -119,7 +119,7 @@ int main(){
 
    printf("\n");
 
-   //addition of unsigned
+   printf("//addition of unsigned\n");
    uca=0xFE;
    ucb=0x3;
    uctmp = uca + ucb;
@@ -127,13 +127,13 @@ int main(){
    show_bytes((pointer)&ucb,sizeof(unsigned char));
    show_bytes((pointer)&uctmp,sizeof(unsigned char));
 
-   //equivalent to getting the (x+y) mod 2^w 
+   printf("//equivalent to getting the (x+y) mod 2^w \n");
    uctmp = (uca + ucb) % (1 << 8);
    show_bytes((pointer)&uctmp,sizeof(unsigned char));
 
    printf("\n");
 
-   //addition of signed
+   printf("//addition of signed\n");
    sca=60;
    scb=-80;
    sctmp=sca+scb;
@@ -141,7 +141,7 @@ int main(){
    show_bytes((pointer)&scb,sizeof(char));
    show_bytes((pointer)&sctmp,sizeof(char));
   
-   //signed addition using properties in relation to unsigned 
+   printf("//signed addition using properties in relation to unsigned\n");
    uca = sca;
    ucb = scb;
    uctmp = uca+ucb;
@@ -149,7 +149,8 @@ int main(){
    show_bytes((pointer)&sctmp,sizeof(char));
   
    printf("\n");
-   //unsigned multiplication 
+   
+   printf("//unsigned multiplication\n");
    uca=0x9B;
    ucb=0x5;
    uctmp = uca * ucb; 
@@ -157,13 +158,13 @@ int main(){
    show_bytes((pointer)&ucb,sizeof(unsigned char));
    show_bytes((pointer)&uctmp,sizeof(unsigned char));
    
-   //equivalent to getting the (x*y) mod 2^w 
+   printf("//equivalent to getting the (x*y) mod 2^w \n");
    uctmp = (uca * ucb) % (1 << 8);
    show_bytes((pointer)&uctmp,sizeof(unsigned char));
    
    printf("\n");
    
-   //signed multiplication 
+   printf("//signed multiplication\n"); 
    sca=-50;
    scb=5;
    sctmp = sca * scb; 
@@ -171,13 +172,13 @@ int main(){
    show_bytes((pointer)&scb,sizeof(char));
    show_bytes((pointer)&sctmp,sizeof(char));
    
-   //equivalent to getting the (x*y) mod 2^w 
+   printf("//equivalent to getting the (x*y) mod 2^w \n");
    sctmp = (sca * scb) % (1 << 8);
    show_bytes((pointer)&sctmp,sizeof(char));
    
    printf("\n");
 
-   //power of two multiply with shift 3 * 2^4
+   printf("//power of two multiply with shift 3 * 2^4\n");
    uca= 3 << 4;
    ucb = 3 * (2  * 2 * 2 * 2 );
    show_bytes((pointer)&uca,sizeof(unsigned char));
@@ -185,7 +186,7 @@ int main(){
    
    printf("\n");
   
-   //power of two multiply with shift -3 * 2^4
+   printf("//power of two multiply with shift -3 * 2^4\n");
    sca= -3 << 4;
    scb = -3 * (2  * 2 * 2 * 2 );
    show_bytes((pointer)&sca,sizeof(char));
@@ -193,7 +194,7 @@ int main(){
    
    printf("\n");
 
-   //signed power of two divide with shift
+   printf("//signed power of two divide with shift\n");
    //-66 / (2^4) = 66 >> 4
 
    sca = -66 / (2*2*2*2);
