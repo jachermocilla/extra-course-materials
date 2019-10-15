e616af6c99b371252be7eaad6b19924c6b864a44
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-10-16 06:29:08 +0800

    edits

--- cmsc131/swapa.c
+++ cmsc131/swapa.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swapa(long *xp, long *yp){
+void swap(long *xp, long *yp){
    volatile long loc[2];
    loc[0] = *xp;
    loc[1] = *yp;
@@ -9,12 +9,24 @@ void swapa(long *xp, long *yp){
    *yp = loc[0];
 }
 
+void swap_ele(long a[], int i){
+   swap(&a[i], &a[i+1]);
+}
+
+long sum = 0;
+void swap_ele_sum(long a[], int i){
+   swap(&a[i],&a[i+1]);
+   sum += (a[i]*a[i+1]);
+}
+
+
+
 int main(){
    long a=123;
    long b=456;
    
    printf("a=%ld,b=%ld\n",a,b);
-   swapa(&a,&b);
+   swap(&a,&b);
    printf("a=%ld,b=%ld\n",a,b);
 
 
