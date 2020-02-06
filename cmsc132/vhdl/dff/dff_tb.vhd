3325f8e6e4132a5a8be5680e5f25810cc48cb7b1
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-02-06 13:12:49 +0800

    edits

--- cmsc132/vhdl/dff/dff_tb.vhd
+++ cmsc132/vhdl/dff/dff_tb.vhd
@@ -5,15 +5,14 @@ ENTITY DFF_tb IS
 END DFF_tb;
 
 ARCHITECTURE behavior OF DFF_tb IS 
-
-    COMPONENT DFF
-    PORT(
+   COMPONENT DFF
+      PORT(
          din : IN  std_logic;
          clk : IN  std_logic;
          rst : IN  std_logic;
          dout : OUT  std_logic
-        ); 
-    END COMPONENT;
+      ); 
+   END COMPONENT;
     
    signal din : std_logic := '0';
    signal clk : std_logic := '0';
@@ -22,37 +21,34 @@ ARCHITECTURE behavior OF DFF_tb IS
 
    constant clk_period : time := 10 ns;
 
-BEGIN
+   BEGIN
 
- uut: DFF PORT MAP (
+   uut: DFF PORT MAP (
           din => din,
           clk => clk,
           rst => rst,
           dout => dout
         );
 
-  clk_process :process
-  begin
-  clk <= '0';
-  wait for clk_period/2;
-  clk <= '1';
-  wait for clk_period/2;
-  end process;
+   clk_process :process
+   begin
+      clk <= '0';
+      wait for clk_period/2;
+      clk <= '1';
+      wait for clk_period/2;
+   end process;
 
-  stim_proc: process
-  begin  
-
-  rst <= '1';
-  wait for 50 ns; 
-
-  rst <= '0';
-  din <= '0';
-  wait for 50 ns;
+   stim_proc: process
+   begin  
+      rst <= '1';
+      wait for 50 ns; 
+   
+      rst <= '0';
+      din <= '0';
+      wait for 50 ns;
   
-  rst <= '0';
-  din <= '1';  
-  wait;
-  
-  end process;
-
+      rst <= '0';
+      din <= '1';  
+      wait;
+   end process;
 END;
