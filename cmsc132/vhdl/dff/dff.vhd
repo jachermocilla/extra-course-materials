40e0cbc9d9b423335b0dd9c53b8bc3dab5ee65a5
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-02-06 13:09:27 +0800

    edits

--- cmsc132/vhdl/dff/dff.vhd
+++ cmsc132/vhdl/dff/dff.vhd
@@ -3,20 +3,19 @@ use ieee.std_logic_1164.all;
 
 entity DFF is
 port( din: in std_logic;
-clk: in std_logic;
-rst: in std_logic;
-dout: out std_logic);
+      clk: in std_logic;
+      rst: in std_logic;
+      dout: out std_logic);
 end DFF;
 
 architecture behavioral of DFF is
 begin
-process(rst,clk,din)
-begin
-if (rst='1') then
-dout<='0';
-elsif(rising_edge(clk)) then
-dout<= din;
-end if;
-end process;
-
+   process(rst,clk,din)
+      begin
+         if (rst='1') then
+         dout<='0';
+      elsif(rising_edge(clk)) then
+         dout<= din;
+      end if;
+   end process;
 end behavioral;
