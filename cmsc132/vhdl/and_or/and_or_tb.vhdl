b980d1dd6dbe674675355a8654eb949ba1c7cf72
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-20 16:39:12 +0800

    edits

--- cmsc132/vhdl/and_or/and_or_tb.vhdl
+++ cmsc132/vhdl/and_or/and_or_tb.vhdl
@@ -21,10 +21,10 @@ BEGIN
    BEGIN
       input <= "000"; WAIT FOR 10 NS; ASSERT output='0' REPORT "000 failed,output= " & STD_LOGIC'IMAGE(output);
       input <= "001"; WAIT FOR 10 NS; ASSERT output='0' REPORT "001 failed,output= " & STD_LOGIC'IMAGE(output);
-      input <= "010"; WAIT FOR 10 NS; ASSERT output='0' REPORT "011 failed,output= " & STD_LOGIC'IMAGE(output);
+      input <= "010"; WAIT FOR 10 NS; ASSERT output='0' REPORT "010 failed,output= " & STD_LOGIC'IMAGE(output);
       input <= "011"; WAIT FOR 10 NS; ASSERT output='1' REPORT "011 failed,output= " & STD_LOGIC'IMAGE(output);
       input <= "100"; WAIT FOR 10 NS; ASSERT output='0' REPORT "100 failed,output= " & STD_LOGIC'IMAGE(output);
-      input <= "101"; WAIT FOR 10 NS; ASSERT output='1' REPORT "100 failed,output= " & STD_LOGIC'IMAGE(output);
+      input <= "101"; WAIT FOR 10 NS; ASSERT output='1' REPORT "101 failed,output= " & STD_LOGIC'IMAGE(output);
       input <= "110"; WAIT FOR 10 NS; ASSERT output='1' REPORT "110 failed,output= " & STD_LOGIC'IMAGE(output);
       input <= "111"; WAIT FOR 10 NS; ASSERT output='1' REPORT "111 failed,output= " & STD_LOGIC'IMAGE(output);
       WAIT;
