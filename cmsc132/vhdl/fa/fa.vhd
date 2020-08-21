66a21ffb349de372025a9f7e34960d6fde2688a6
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-21 12:29:36 +0800

    edits

--- cmsc132/vhdl/fa/fa.vhd
+++ cmsc132/vhdl/fa/fa.vhd
@@ -1,19 +1,17 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
  
-entity full_adder_vhdl_code is
- Port ( A : in STD_LOGIC;
- B : in STD_LOGIC;
- Cin : in STD_LOGIC;
- S : out STD_LOGIC;
- Cout : out STD_LOGIC);
-end full_adder_vhdl_code;
+ENTITY full_adder is
+ PORT ( a : IN STD_LOGIC;
+ b : IN STD_LOGIC;
+ CarryIn : IN STD_LOGIC;
+ Sum : OUT STD_LOGIC;
+ Cout : OUT STD_LOGIC);
+END full_adder;
  
-architecture gate_level of full_adder_vhdl_code is
+ARCHITECTURE gate_level OF full_adder IS
  
-begin
- 
+BEGIN
  S <= A XOR B XOR Cin ;
  Cout <= (A AND B) OR (Cin AND A) OR (Cin AND B) ;
- 
 end gate_level;
