076195e312c244a28b4377c9fe93fdbce96bdeaf
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-21 13:22:04 +0800

    edits

--- cmsc132/vhdl/21mux/mux_2to1_tb.vhd
+++ cmsc132/vhdl/21mux/mux_2to1_tb.vhd
@@ -4,25 +4,25 @@ USE ieee.std_logic_1164.all;
 ENTITY mux_2to1_tb IS
 END mux_2to1_tb;
 
-architecture behavior of mux_2to1_tb is
-   component mux_2to1 is 
-      port (
+ARCHITECTURE behavior OF mux_2to1_tb IS
+   COMPONENT mux_2to1 IS 
+      PORT (
          A, B, S: IN STD_LOGIC;
          C: OUT STD_LOGIC);
-   end component;
-   signal input: STD_LOGIC_VECTOR(2 downto 0);
-   signal output: STD_LOGIC;
+   END COMPONENT;
+   SIGNAL input: STD_LOGIC_VECTOR(2 DOWNTO 0);
+   SIGNAL output: STD_LOGIC;
 
-begin 
-   uut: mux_2to1 port map (
+BEGIN 
+   uut: mux_2to1 PORT MAP (
       A => input(1),
       B => input(2),
       S => input(0),
       C => output
    );
 
-   stim_proc: process
-   begin
+   stim_proc: PROCESS
+   BEGIN
       input <= "000"; wait for 10 ns; assert output='0' report "000 failed,output= " & std_logic'image(output);
       input <= "001"; wait for 10 ns; assert output='0' report "001 failed,output= " & std_logic'image(output);
       input <= "010"; wait for 10 ns; assert output='1' report "010 failed,output= " & std_logic'image(output); 
@@ -31,8 +31,8 @@ begin
       input <= "101"; wait for 10 ns; assert output='1' report "101 failed,output= " & std_logic'image(output); 
       input <= "110"; wait for 10 ns; assert output='0' report "110 failed,output= " & std_logic'image(output);
       input <= "111"; wait for 10 ns; assert output='1' report "111 failed,output= " & std_logic'image(output);
-      wait;
-   end process;
-end;      
+      WAIT;
+   END PROCESS;
+END;      
    
 
