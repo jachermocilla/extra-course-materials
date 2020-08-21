7a92903b8d9d82bba9c59a63d3ec48ea204e7db7
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-21 13:35:47 +0800

    edits

--- cmsc132/vhdl/21mux/mux_2to1_tb.vhd
+++ cmsc132/vhdl/21mux/mux_2to1_tb.vhd
@@ -23,14 +23,8 @@ BEGIN
 
    stim_proc: PROCESS
    BEGIN
-      input <= "000"; wait for 10 ns; assert output='0' report "000 failed,output= " & std_logic'image(output);
-      input <= "001"; wait for 10 ns; assert output='0' report "001 failed,output= " & std_logic'image(output);
-      input <= "010"; wait for 10 ns; assert output='1' report "010 failed,output= " & std_logic'image(output); 
-      input <= "011"; wait for 10 ns; assert output='1' report "011 failed,output= " & std_logic'image(output);
-      input <= "100"; wait for 10 ns; assert output='0' report "100 failed,output= " & std_logic'image(output);
-      input <= "101"; wait for 10 ns; assert output='1' report "101 failed,output= " & std_logic'image(output); 
-      input <= "110"; wait for 10 ns; assert output='0' report "110 failed,output= " & std_logic'image(output);
-      input <= "111"; wait for 10 ns; assert output='1' report "111 failed,output= " & std_logic'image(output);
+      input <= "010"; wait for 10 ns; assert output='1' report "000 failed,output= " & std_logic'image(output);
+      input <= "101"; wait for 10 ns; assert output='1' report "001 failed,output= " & std_logic'image(output);
       WAIT;
    END PROCESS;
 END;      
