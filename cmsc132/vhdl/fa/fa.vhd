1edd9fcd6798d67b7472c289547f6836a882cc18
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-21 12:31:01 +0800

    edits

--- cmsc132/vhdl/fa/fa.vhd
+++ cmsc132/vhdl/fa/fa.vhd
@@ -2,16 +2,16 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
  
 ENTITY full_adder is
- PORT ( a : IN STD_LOGIC;
- b : IN STD_LOGIC;
- CarryIn : IN STD_LOGIC;
- Sum : OUT STD_LOGIC;
- Cout : OUT STD_LOGIC);
+   PORT ( a : IN STD_LOGIC;
+      b : IN STD_LOGIC;
+      CarryIn : IN STD_LOGIC;
+      Sum : OUT STD_LOGIC;
+      CarryOut : OUT STD_LOGIC);
 END full_adder;
  
 ARCHITECTURE gate_level OF full_adder IS
  
 BEGIN
- S <= A XOR B XOR Cin ;
- Cout <= (A AND B) OR (Cin AND A) OR (Cin AND B) ;
+ S <= A XOR B XOR CarryIn ;
+ CarryOut <= (A AND B) OR (CarrayIn AND A) OR (CarrayOut AND B) ;
 end gate_level;
