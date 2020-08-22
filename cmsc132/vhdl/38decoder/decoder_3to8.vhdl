7bde76a3bd8d83ff9c730f5048f9c7b83eaf7af9
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-22 17:14:29 +0800

    edits

--- cmsc132/vhdl/38decoder/decoder_3to8.vhdl
+++ cmsc132/vhdl/38decoder/decoder_3to8.vhdl
@@ -3,20 +3,20 @@ USE ieee.std_logic_1164.all;
 ---------------------------------
 ENTITY decoder_3to8 IS
    PORT (
-      INPUT: IN STD_LOGIC_VECTOR(2 downto 0);
-      OUTPUT: OUT STD_LOGIC_VECTOR(7 downto 0)
+      I2, I1, I0: IN STD_LOGIC);
+      Out7, Out6, Out5, Out4, Out3, Out2, Out1, Out0: OUT STD_LOGIC;
    );
 END decoder_3to8;
 --------------------------------
 
 ARCHITECTURE pure_logic OF decoder_3to8 IS
 BEGIN
-   OUTPUT(0) <= NOT INPUT(2) AND NOT INPUT(1) and NOT INPUT(0);
-   OUTPUT(1) <= NOT INPUT(2) AND NOT INPUT(1) and INPUT(0);
-   OUTPUT(2) <= NOT INPUT(2) AND INPUT(1) and NOT INPUT(0);
-   OUTPUT(3) <= NOT INPUT(2) AND INPUT(1) and INPUT(0);
-   OUTPUT(4) <= INPUT(2) AND NOT INPUT(1) and NOT INPUT(0);
-   OUTPUT(5) <= INPUT(2) AND NOT INPUT(1) and INPUT(0);
-   OUTPUT(6) <= INPUT(2) AND INPUT(1) and NOT INPUT(0);
-   OUTPUT(7) <= INPUT(2) AND INPUT(1) and INPUT(0);
+   Out0 <= NOT INPUT(2) AND NOT INPUT(1) and NOT INPUT(0);
+   Out1 <= NOT INPUT(2) AND NOT INPUT(1) and INPUT(0);
+   Out2 <= NOT INPUT(2) AND INPUT(1) and NOT INPUT(0);
+   Out3 <= NOT INPUT(2) AND INPUT(1) and INPUT(0);
+   Out4 <= INPUT(2) AND NOT INPUT(1) and NOT INPUT(0);
+   Out5 <= INPUT(2) AND NOT INPUT(1) and INPUT(0);
+   Out6 <= INPUT(2) AND INPUT(1) and NOT INPUT(0);
+   Out7 <= INPUT(2) AND INPUT(1) and INPUT(0);
 END pure_logic;
