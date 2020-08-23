b8355ea672874ea8e10ddbac0ad4249853d241e2
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 12:21:20 +0800

    edits

--- cmsc132/vhdl/ALU_1bit/alu_tb.vhdl
+++ cmsc132/vhdl/ALU_1bit/alu_tb.vhdl
@@ -1,9 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-
 ENTITY alu_tb IS
 END alu_tb;
-
 ARCHITECTURE behavior OF alu_tb IS
    COMPONENT alu IS
       PORT (a, b, CarryIn: IN STD_LOGIC;
@@ -24,21 +22,15 @@ BEGIN
    );
    STIM_PROC: PROCESS
    BEGIN
-
       -- 1 AND 1
       input <= "00011"; WAIT FOR 10 NS; ASSERT res='1' REPORT "00011 failed,res= " & STD_LOGIC'IMAGE(res);
-
       -- 1 OR 1
       input <= "01011"; WAIT FOR 10 NS; ASSERT res='1' REPORT "01011 failed,res= " & STD_LOGIC'IMAGE(res);
-
       -- 1 + 1
       input <= "10011"; WAIT FOR 10 NS; ASSERT res='0' REPORT "10011 failed,res= " & STD_LOGIC'IMAGE(res);
-      
       -- 1 + 0
       input <= "10010"; WAIT FOR 10 NS; ASSERT res='1' REPORT "10011 failed,res= " & STD_LOGIC'IMAGE(res);
       WAIT;
-
-
    END PROCESS;
 END;      
    
