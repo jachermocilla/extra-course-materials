db202fcc69596e023f5dff46483df14fdea6c530
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 12:00:44 +0800

    edits

--- cmsc132/vhdl/21mux/mux_4to1_tb.vhd
+++ cmsc132/vhdl/21mux/mux_4to1_tb.vhd
@@ -15,27 +15,21 @@ ARCHITECTURE behavior OF mux_4to1_tb IS
 
 BEGIN 
    uut: mux_4to1 PORT MAP (
-      S0 => input(0),
-      S1 => input(1),
-      A => input(2),
-      B => input(3),
-      C => input(4),
-      D => input(5),
+      S0 => input(5),
+      S1 => input(4),
+      A => input(3),
+      B => input(2),
+      C => input(1),
+      D => input(0),
       E => output
    );
 
    stim_proc: PROCESS
    BEGIN
-      input <= "001010"; wait for 10 ns; assert output='1' report "00 failed,output= " & std_logic'image(output);
-      WAIT;
-
-      input <= "011010"; wait for 10 ns; assert output='0' report "01 failed,output= " & std_logic'image(output);
-      WAIT;
-
-      input <= "101010"; wait for 10 ns; assert output='1' report "10 failed,output= " & std_logic'image(output);
-      WAIT;
-
-      input <= "111010"; wait for 10 ns; assert output='0' report "11 failed,output= " & std_logic'image(output);
+      input <= "001011"; wait for 10 ns; assert output='1' report "00 failed,output= " & std_logic'image(output);
+      input <= "011011"; wait for 10 ns; assert output='0' report "01 failed,output= " & std_logic'image(output);
+      input <= "101011"; wait for 10 ns; assert output='1' report "10 failed,output= " & std_logic'image(output);
+      input <= "111011"; wait for 10 ns; assert output='0' report "11 failed,output= " & std_logic'image(output);
       WAIT;
    END PROCESS;
 END;      
