7f792cdbf8179a378c2e67c3da54159dd22eb85b
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 13:11:11 +0800

    edits

--- cmsc132/vhdl/21mux/mux_4bit_tb.vhdl
+++ cmsc132/vhdl/21mux/mux_4bit_tb.vhdl
@@ -10,35 +10,22 @@ ARCHITECTURE behavior OF mux_4bit_tb IS
          S: IN STD_LOGIC;
          C: OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
    END COMPONENT;
-   SIGNAL input_A: STD_LOGIC_VECTOR(3 DOWNTO 0);
-   SIGNAL input_B: STD_LOGIC_VECTOR(3 DOWNTO 0);
+   SIGNAL input_A, input_B : STD_LOGIC_VECTOR(3 DOWNTO 0);
    SIGNAL input_S: STD_LOGIC;
    SIGNAL output_C: STD_LOGIC_VECTOR(3 DOWNTO 0);
-
 BEGIN 
    uut: mux_4bit PORT MAP (
-      S => input_S,
-      A => input_A,
-      B => input_B,
+      S => input_S, A => input_A, B => input_B,
       C => output_C
    );
-
    stim_proc: PROCESS
    BEGIN
-      input_A <= "0011";
-      input_B <= "1000";
-      input_S <= '0';
-      WAIT FOR 10 ns;
-      ASSERT output_C="0011";   
+      input_A <= "0011"; input_B <= "1000"; input_S <= '0';
+      WAIT FOR 10 ns; ASSERT output_C="0011";   
       
-      input_A <= "0011";
-      input_B <= "1000";
-      input_S <= '1';
-      WAIT FOR 10 ns;
-      ASSERT output_C="1000";   
-
+      input_A <= "0011"; input_B <= "1000"; input_S <= '1';
+      WAIT FOR 10 ns; ASSERT output_C="1000";   
       WAIT;
-      
    END PROCESS;
 END;      
    
