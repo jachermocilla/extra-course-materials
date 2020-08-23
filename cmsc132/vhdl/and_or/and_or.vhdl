feeae04fba0844d63a3a296fc9b5d749a1c260d8
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 10:53:49 +0800

    edits

--- cmsc132/vhdl/and_or/and_or.vhdl
+++ cmsc132/vhdl/and_or/and_or.vhdl
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all;
 ---------------------------------
 ENTITY and_or IS
    PORT (a, b, Operation: IN STD_LOGIC;
-   result: OUT STD_LOGIC);
+   Result: OUT STD_LOGIC);
 END and_or;
 --------------------------------
 ARCHITECTURE behavioral OF and_or IS
@@ -24,5 +24,5 @@ ARCHITECTURE behavioral OF and_or IS
 BEGIN
    u1: and_gate port map(a, b, u);
    u2: or_gate port map(a, b, v);
-   u3: mux_2to1 port map(u, v, Operation, result); 
+   u3: mux_2to1 port map(u, v, Operation, iResult); 
 END behavioral;
