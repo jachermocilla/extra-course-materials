b2c0b26ea9f0a655385f332ac037e0a2624e37c0
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 11:11:50 +0800

    edits

--- cmsc132/vhdl/ALU_1bit/alu.vhdl
+++ cmsc132/vhdl/ALU_1bit/alu.vhdl
@@ -2,7 +2,8 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 ---------------------------------
 ENTITY alu IS
-   PORT (a, b, CarryIn, S0, S1: IN STD_LOGIC;
+   PORT (a, b, CarryIn : IN STD_LOGIC;
+   Operation: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
    Result, CarryOut: OUT STD_LOGIC);
 END alu;
 --------------------------------
@@ -27,5 +28,5 @@ BEGIN
    u1: and_gate port map(a, b, u);
    u2: or_gate port map(a, b, v);
    u3: full_adder port map(a, b, CarryIn, w, CarryOut); 
-   u4: mux_4to1 port map(u, v, w, x, S0, S1, Result);
+   u4: mux_4to1 port map(u, v, w, x, Operation(0), Operation(1), Result);
 END behavioral;
