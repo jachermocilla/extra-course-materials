dd649e32b6f749516a9fc758492483058846d18e
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-23 11:51:45 +0800

    edits

--- cmsc132/vhdl/ALU_1bit/alu_tb.vhdl
+++ cmsc132/vhdl/ALU_1bit/alu_tb.vhdl
@@ -14,11 +14,11 @@ ARCHITECTURE behavior OF alu_tb IS
    SIGNAL res, cout: STD_LOGIC;
 BEGIN 
    UUT: alu PORT MAP (
-      Operation(0) => input(0),
-      Operation(1) => input(1),
+      Operation(0) => input(4),
+      Operation(1) => input(3),
       CarryIn => input(2),
-      a => input(3),
-      b => input(4),
+      a => input(1),
+      b => input(0),
       Result => res,
       CarryOut => cout
    );
@@ -29,10 +29,10 @@ BEGIN
       input <= "00011"; WAIT FOR 10 NS; ASSERT res='1' REPORT "00011 failed,res= " & STD_LOGIC'IMAGE(res);
 
       -- 1 OR 1
-      input <= "01011"; WAIT FOR 10 NS; ASSERT res='1' REPORT "01011 failed,res= " & STD_LOGIC'IMAGE(res);
+      --input <= "01011"; WAIT FOR 10 NS; ASSERT res='1' REPORT "01011 failed,res= " & STD_LOGIC'IMAGE(res);
 
       -- 1 + 1
-      input <= "10011"; WAIT FOR 10 NS; ASSERT res='0' REPORT "01011 failed,res= " & STD_LOGIC'IMAGE(res);
+      --input <= "10011"; WAIT FOR 10 NS; ASSERT res='0' REPORT "10011 failed,res= " & STD_LOGIC'IMAGE(res);
       WAIT;
 
 
