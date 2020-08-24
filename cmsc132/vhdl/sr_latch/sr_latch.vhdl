db6e48587a3bb00643df05966ac7216e27c9edc8
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-24 17:02:02 +0800

    edits

---
+++ cmsc132/vhdl/sr_latch/sr_latch.vhdl
@@ -0,0 +1,14 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+---------------------------------
+ENTITY sr_latch IS
+   PORT (R, S: IN STD_LOGIC;
+   Q, Q_BAR: INOUT STD_LOGIC);
+END sr_latch;
+--------------------------------
+ARCHITECTURE pure_logic OF sr_latch IS
+   SIGNAL tmp: STD_LOGIC;
+BEGIN
+   Q <= R NOR Q_BAR;
+   Q_BAR <= S NOR Q;
+END pure_logic;
