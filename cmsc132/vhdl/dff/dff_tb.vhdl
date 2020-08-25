1b8f53c0d071b3d674caae2fe82eedfac6262816
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-08-25 17:07:20 +0800

    edits

--- cmsc132/vhdl/dff/dff_tb.vhdl
+++ cmsc132/vhdl/dff/dff_tb.vhdl
@@ -3,18 +3,15 @@ USE ieee.std_logic_1164.all;
 ENTITY dff_tb IS
 END dff_tb;
 ARCHITECTURE behavior OF dff_tb IS
-   --100Mhz
    CONSTANT frequency: integer := 100e6; 
    CONSTANT period : time := 1000 ms / frequency;
    SIGNAL clk : std_logic := '0';
-
    COMPONENT dff is
    PORT( D: IN STD_LOGIC;
       C: IN STD_LOGIC;
       Q: INOUT STD_LOGIC;
       Q_BAR: INOUT STD_LOGIC);
    END COMPONENT;
-
    SIGNAL D: STD_LOGIC := '0';
    SIGNAL Q: STD_LOGIC := '0';
    SIGNAL Q_BAR: STD_LOGIC;
@@ -25,12 +22,11 @@ BEGIN
    BEGIN
       D <= '0';
       WAIT FOR period / 2 ;
-
       D <= '1';
       WAIT FOR 20 ns;
-
       D <= '0';
-
+      WAIT FOR 20 ns;
+      D <= '1';
       WAIT;
    END PROCESS;
 END ARCHITECTURE;
