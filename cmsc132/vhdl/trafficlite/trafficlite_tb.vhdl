567df97184f50760418b78739168fbcd69ffa43a
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-09-01 18:31:29 +0800

    edits

--- cmsc132/vhdl/trafficlite/trafficlite_tb.vhdl
+++ cmsc132/vhdl/trafficlite/trafficlite_tb.vhdl
@@ -6,18 +6,14 @@ ARCHITECTURE behavior OF trafficlite_tb IS
    --100Mhz
    CONSTANT frequency: integer := 100e6; 
    CONSTANT period : time := 1000 ms / frequency;
-
    COMPONENT trafficlite IS
-   PORT (
-      EWCar, NSCar, clk: IN STD_LOGIC;
+   PORT (EWCar, NSCar, clk: IN STD_LOGIC;
       EWLite, NSLite: OUT STD_LOGIC
    );
    END COMPONENT;
-
    SIGNAL clk : STD_LOGIC := '0';
    SIGNAL inputs : STD_LOGIC_VECTOR(1 DOWNTO 0);
    SIGNAL outputs : STD_LOGIC_VECTOR(1 DOWNTO 0);
-
 BEGIN 
    clk <= NOT clk AFTER period / 2;
    uut: trafficlite PORT MAP (inputs(1),inputs(0),clk,outputs(1),outputs(0));
@@ -25,13 +21,9 @@ BEGIN
    BEGIN
       inputs <= "00";
       WAIT FOR period / 2;
-      
       inputs <= "10";
       WAIT FOR period / 2;
-      
       inputs <= "01";
       WAIT FOR period / 2;
-     
-      WAIT; 
    END PROCESS;
 END ARCHITECTURE;
