cab84fdf4c3cef38689b0df7cc0f50bdd5bea891
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2020-09-02 17:30:23 +0800

    edits

--- cmsc132/vhdl/reg_file/reg_file2.vhdl
+++ cmsc132/vhdl/reg_file/reg_file2.vhdl
@@ -14,17 +14,17 @@ ENTITY reg_file2 is
       read_data2: OUT STD_LOGIC);
 END reg_file2;
 ARCHITECTURE behavioral OF reg_file2 IS
-TYPE rf_type IS ARRAY(0 to 1) of std_logic;
+TYPE rf_type IS ARRAY(0 to 1) of STD_LOGIC;
 SIGNAL registers : rf_type;
 BEGIN
    rf: PROCESS(clk)
    BEGIN
       IF RISING_EDGE(clk) THEN
          IF (write ='1') THEN
-            registers(to_integer(unsigned'('0' & write_n))) <= write_data;
+            registers(TO_INTEGER(UNSIGNED'('0' & write_n))) <= write_data;
          END IF;
       END IF;
-      read_data1 <= registers(to_integer(unsigned'( '0' & read_n1)));
-      read_data2 <= registers(to_integer(unsigned'( '0' & read_n2)));
+      read_data1 <= registers(TO_INTEGER(UNSIGNED'( '0' & read_n1)));
+      read_data2 <= registers(TO_INTEGER(UNSIGNED'( '0' & read_n2)));
    END PROCESS;
 END behavioral;
