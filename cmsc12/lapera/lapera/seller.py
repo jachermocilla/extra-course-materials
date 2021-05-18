85734259022426ac81d7d31a701c3832d4c0397a
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2021-05-18 16:44:35 +0800

    seems to be working now

--- cmsc12/lapera/lapera/seller.py
+++ cmsc12/lapera/lapera/seller.py
@@ -32,7 +32,7 @@ def create_seller_dict( seller_id,
 def load_seller_db():
     seller_db_handle = open("data/seller.db","r")
     lines = seller_db_handle.readlines()
-    sellers=[]
+    sellers.clear()
     count = 0
     for line in lines:
         count += 1        
@@ -55,13 +55,13 @@ def seller_init():
     load_seller_db()
     
 def save_seller_dict(seller_dict):
-    seller_db_handle = open("data/seller.db","r+")
+    seller_db_handle = open("data/seller.db","a+")
     
     output_line = str(seller_dict["seller_id"]+","+
                     seller_dict["seller_first_name"]+","+ 
                     seller_dict["seller_last_name"]+","+ 
                     seller_dict["seller_login_name"]+","+ 
-                    seller_dict["seller_password_hash"]) 
+                    seller_dict["seller_password_hash"]+"\n") 
     print(output_line)
     seller_db_handle.write(output_line)
     seller_db_handle.close()
@@ -69,9 +69,10 @@ def save_seller_dict(seller_dict):
 def register_seller():
     print("<<Register Seller>>")
     new_seller_dict = {}
-    new_seller_dict["seller_first_name"] = str(input("First Name:"))
-    new_seller_dict["seller_last_name"] = str(input("Last Name:"))
-    new_seller_dict["seller_login_name"] = str(input("Login Name:"))
+    new_seller_dict['seller_id'] = str(len(sellers))
+    new_seller_dict["seller_first_name"] = str(input("First Name: "))
+    new_seller_dict["seller_last_name"] = str(input("Last Name: "))
+    new_seller_dict["seller_login_name"] = str(input("Login Name: "))
     
     #TODO: check of the user exists in the seller file
     matched = False
@@ -85,6 +86,7 @@ def register_seller():
             matched = True
     new_seller_dict["seller_password_hash"] = password1
     print(new_seller_dict)
+    save_seller_dict(new_seller_dict)
 
 def register_shopper():
     print("Register Shopper")
